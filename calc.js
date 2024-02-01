07716df54750255fd7fca441b1e2336914707423
kwtkd <kiwatakda@gmail.com>
2024-02-01 21:02:25 +0900

    再提出kadai_007

--- calc.js
+++ calc.js
@@ -1,19 +1,10 @@
-// 変数(num)が3の倍数の場合： “3の倍数です”
-num = 3;
-if (num % 3 === 0){
-  console.log('3の倍数です');
-}
-
-// 変数(num)が5の倍数の場合： “5の倍数です”
-num = 5;
-if (num % 5 === 0){
-  console.log('5の倍数です');
-}
-
-// 変数(num)が3と5の倍数の場合： “3と5の倍数です”
-num = 3;
+num = 10;
 if (num % 3 === 0 && num % 5 === 0){
   console.log('3と5の倍数です');
+}else if (num % 3 === 0){
+  console.log('3の倍数です');
+} else if(num % 5 === 0){
+  console.log('5の倍数です');
 } else{
   console.log(num);
 } 

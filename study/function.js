7674463bcb7171510f47b0a3d67b2b766359a13c
kwtkd <kiwatakda@gmail.com>
2024-02-02 09:49:45 +0900

    kadai_014提出

---
+++ study/function.js
@@ -0,0 +1,27 @@
+const calculateTotal = (price) => {
+  console.log(price + 500 + '円');
+}
+
+calculateTotal(1200);
+
+const sayGoodMorning = () => {
+  console.log('おはようございます');
+}
+
+sayGoodMorning();
+
+const addTwoArguments = (price , shippingfee) => {
+  console.log (price + shippingfee + '円');
+}
+
+addTwoArguments(1200 , 500);
+
+const double = (num) => {
+  return num *2;
+} 
+
+console.log(double(30));
+
+const userName = '太郎';
+
+console.log(userName);

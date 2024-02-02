cffded4ba2de3502a3e87f56ee60f920a7586024
kwtkd <kiwatakda@gmail.com>
2024-02-02 10:03:33 +0900

    再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,2 @@
 var today = new Date();
-console.log(today.getFullYear() + '年');
-console.log(today.getMonth() + 1  + '月');
-console.log(today.getDate() + '日');
+console.log(today.getFullYear() + '年' + (today.getMonth() + 1)  + '月' + today.getDate() + '日');
